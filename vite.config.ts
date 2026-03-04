21181c875c94838415bcd9709124b87d4a744ed2
Ilarion Petriv <115731701+larik-22@users.noreply.github.com>
2026-03-04 16:57:23 +0100

    feat(vite) with CF tunnel fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // vite.config.js
 export default defineConfig({
-	plugins: [],
-	server: {
-		host: "localhost",
-		port: 3000,
-		cors: true,
-		hmr: {
-			host: "localhost",
-			protocol: "ws",
-		},
-	},
-	build: {
-		minify: true,
-		manifest: true,
-		rollupOptions: {
-			input: "./src/main.ts",
-			output: {
-				format: "umd",
-				entryFileNames: "main.js",
-				esModule: false,
-				compact: true,
-				globals: {
-					jquery: "$",
-				},
-			},
-			external: ["jquery"],
-		},
-	},
-});
+  plugins: [eslintPlugin()],
+  server: {
+    host: 'localhost',
+    cors: '*',
+    allowedHosts: 'all',
+    hmr: {
+      protocol: 'wss',
+      clientPort: 443,
+    },
+  },
+  build: {
+    minify: true,
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.ts',
+      output: {
+        format: 'iife',
+        entryFileNames: 'main.js',
+        inlineDynamicImports: true,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  },
+  optimizeDeps: {
+    include: ['gsap', 'gsap/ScrollTrigger', 'gsap/SplitText'],
+  },
+})
+
